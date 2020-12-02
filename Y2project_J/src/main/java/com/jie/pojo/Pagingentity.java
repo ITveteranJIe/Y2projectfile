e6f67590ca9b952d4fafe2f5983ad0668928a6a2
Jieless-Hub <451360397@qq.com>
2020-12-02 22:33:08 +0800

    pojo/pagingentity 数据实体类

---
+++ Y2project_J/src/main/java/com/jie/pojo/Pagingentity.java
@@ -0,0 +1,11 @@
+package com.jie.pojo;
+
+/**
+ * ♥--?
+ * ♥O Copyright (C) JIe
+ * ♥-Y <author>     <time>             <desc>          <ClassName>
+ * ♥--J Jie   2020/12/222:27     分页数据实体类      Pagingentity
+ * -♥---J
+ */
+public class Pagingentity {
+}
---
+++ Y2project_J/src/main/java/com/jie/pojo/Projectdata/userList.java
@@ -0,0 +1,11 @@
+package com.jie.pojo.Projectdata;
+
+/**
+ * ♥--?
+ * ♥O Copyright (C) JIe
+ * ♥-Y <author>     <time>             <desc>          <ClassName>
+ * ♥--J Jie   2020/12/222:29     用户表实体类      userList
+ * -♥---J
+ */
+public class userList {
+}
