564c2775ffc7fccdd8c18c3f3ddd9d4c376e7b52
Jieless-Hub <451360397@qq.com>
2020-12-02 23:27:17 +0800

    其他文件

--- Y2project_J/src/main/java/com/jie/config/WebConfig.java
+++ Y2project_J/src/main/java/com/jie/config/WebConfig.java
@@ -1,15 +1,15 @@
 package com.jie.config;
-
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 /**
  * ♥--?
  * ♥O Copyright (C) JIe
  * ♥-Y <author>     <time>             <desc>          <ClassName>
- * ♥--J Jie   2020/12/222:18     Web配置文件      WebConfig
+ * ♥--J Jie   2020/12/222:18          Web配置文件         WebConfig
  * -♥---J
  */
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
--- Y2project_J/src/main/java/com/jie/pojo/Projectdata/userList.java
+++ Y2project_J/src/main/java/com/jie/pojo/Projectdata/userList.java
@@ -1,5 +1,7 @@
 package com.jie.pojo.Projectdata;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 /**
  * ♥--?
  * ♥O Copyright (C) JIe
@@ -7,5 +9,8 @@ package com.jie.pojo.Projectdata;
  * ♥--J Jie   2020/12/222:29     用户表实体类      userList
  * -♥---J
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class userList {
 }
